e9ff0b2f4596e09d3e238d9afd03fd619d6e1177
alexyz1103 <alexyz1103@gmail.com>
2022-10-07 19:54:45 +0300

    Убран класс Item реализация методов: size,isEmpty,indexOf,lastIndexOf, contains,toArray,get,set,add2

--- src/main/java/ru/kostylev/ksjavacore/Main.java
+++ src/main/java/ru/kostylev/ksjavacore/Main.java
@@ -1,19 +1,30 @@
 package ru.kostylev.ksjavacore;
 
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         MyList myList = new MyList<>();
         myList.add("Object 1");
         myList.add(10);
         myList.add("Object 3");
+        myList.add("Object 4");
 
 
-        System.out.println(myList);
-        System.out.println(myList.size());
-        System.out.println(myList.isEmpty());
-        System.out.println(myList.get(0));
-        System.out.println(myList.contains("string object"));
+        System.out.println("Консольный вывод коллекции: " + myList);
+        System.out.println("Метод size(): " + myList.size());
+        System.out.println("Метод isEmpty: " + myList.isEmpty());
+        System.out.println("Метод indexOf(): " + myList.indexOf("Object 1"));
+        System.out.println("Метод lastIndexOf(): " + myList.lastIndexOf(10));
+        System.out.println("Метод contains: " + myList.contains("Object 1"));
+        System.out.println("Метод toArray():" + Arrays.toString(myList.toArray()));
+        System.out.println("Метод get(index): " + myList.get(1));
+        System.out.println("Метод set(index): было " + myList.set(1, "100") + " стало: " + myList.get(1));
+        myList.add(6,"new element");
+        System.out.println("Метод add(index, element): " + myList);
+        //myList.remove("Object 1");
+        //System.out.println(myList);
 
     }
 }
--- src/main/java/ru/kostylev/ksjavacore/MyList.java
+++ src/main/java/ru/kostylev/ksjavacore/MyList.java
@@ -1,20 +1,17 @@
 package ru.kostylev.ksjavacore;
 
-import javax.swing.text.html.HTMLDocument;
 import java.util.*;
 
 
-public class MyList<T> implements List<Object> {
-    private int size;
-    private Item[] list;
+public class MyList<T> implements List<T> {
+    private int size; // поле содержащее размер коллекции
+    private Object[] list; // поле содержащие коллекцию ввиде масива
 
+    //Конструктор без параметров, создает массив на 10 пустых элементов
+    //и задает размер коллекции 0
     public MyList() {
         this.size = 0;
-        this.list = new Item[1];
-    }
-
-    private Object[] getList() {
-        return this.list;
+        this.list = new Object[10];
     }
 
     @Override
@@ -27,52 +24,150 @@ public class MyList<T> implements List<Object> {
         return size()==0;
     }
 
+    //метод возвращающий индекс первого вхождения элемента в коллекцию
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (list[i].equals(o)) return i;
+        }
+        return -1;
+    }
+    //метод возвращающий индекс последнего вхождения элемента в коллекцию
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = size-1; i >= 0; i--) {
+            if (list[i].equals(o)) return i;
+        }
+        return -1;
+    }
+
+    //метод для опеределения что объект входит в состав коллекции
+    //реализован с помощью indexOf, если у объекта нет индекса, те -1
+    //вернет false
     @Override
     public boolean contains(Object o) {
-        if (o != null) {
-            for (Item tItem : list) {
-                if (tItem.getItem().equals(o)) return true;
-            }
-        }
-        return false;
+        return this.indexOf(o)!=-1;
     }
-
-    @Override
-    public Iterator<Object> iterator() {
-        //Iterator newArray = new Iterable<>();
-        return null;
-    }
-
+    //Возвращает массив коллекции равный размеру коллекции
     @Override
     public Object[] toArray() {
-        return this.getList();
+        Object[] arr = new Object[size()];
+        for (int i = 0; i < size; i++) {
+            arr[i] = list[i];
+        }
+        return arr;
     }
-
+    //????????????????????????????
     @Override
     public <T1> T1[] toArray(T1[] a) {
         return null;
     }
+    //вспомогательный метод проверяющий вхождение индекса в размер коллекции
+    public boolean checkIndex(int index){
+        return index >= 0 & index <= size;
+    }
 
+    //метод который возвращает элемент коллекции если индекс входит в размер коллекции
     @Override
-    public boolean add(Object item) {
-        if (size == list.length){
-            Item[] newArray = new Item[list.length+1];
-            System.arraycopy(list, 0, newArray, 0, size);
-            list = newArray;
+    public T get(int index) {
+        return (checkIndex(index)? (T) list[index]: null);
+    }
+
+    //метод заменяет старое значение коллекции в указаном индексе на новое,
+    // если индекс выходит за пределы коллекции возвращает null
+    @Override
+    public T set(int index, T element) {
+        if (checkIndex(index)){
+            T old = (T) list[index];
+            list[index] = element;
+            return old;
         }
-        Item newItem = new Item<>(item,size);
-        list[size] = newItem;
-        size++;
+        return null;
+    }
+
+    //Вспомогательный метод который возвращает срез массива коллекции,
+    //если стартовый и конечный индексы входят в размерность коллекции
+    public Object[] slice(int startIndex, int endIndex){
+        Object[] arr = new Object[endIndex-startIndex+1];
+        for (int i = 0; i < endIndex-startIndex+1; i++) {
+            arr[i] = list[startIndex+i];
+        }
+        return arr;
+    }
+    //вспомогательный метод расширяющий массив вдвое
+    public void grow(Object[] arr){
+            Object[] newArr = new Object[list.length*2];
+            for (int i = 0; i < size; i++) {
+                newArr[i] = list[i];
+            }
+            list = newArr;
+    }
+    //вспомогательный метод проверяет надо ли расширять массив
+    public boolean checkGrow(Object[] arr){
+        return arr.length == size;
+    }
+
+    //метод вставляет элемент в коллекцию в указанной позиции индекса,
+    // если индекс входит в диапазон, со смещением остальных элементов
+    // в право
+    @Override
+    public void add(int index, Object element) {
+        if (checkIndex(index)){
+            if (checkGrow(list)) {grow(list);}
+            Object[] arr = slice(index,size-1);
+            list[index] = element;
+            for (int i = 0; i < arr.length; i++) {
+                list[index+i+1] = arr[i];
+            }
+            size++;
+        }
+    }
+
+    //Метод для добавления элемента коллекции в конец
+    @Override
+    public boolean add(Object o) {
+        if (checkGrow(list)) {grow(list);}
+        add(size,o);
+//        if (size == list.length){ //проверяем если размер коллекции уже равен размеру массива
+//            Object[] newArray = new Object[list.length*2]; // создаем новый массив с размером в два раза больше
+//            for (int i = 0; i < list.length; i++) { newArray[i] = list[i]; } // копируем из массива коллекции в новый расширеный
+//            this.list = newArray;//
+//        }
+//        list[size] = o;
+//        size++;
         return true;
     }
 
     @Override
     public boolean remove(Object o) {
         if (this.contains(o)){
-
+            for (int i = 0; i < size; i++) {
+                if (list[i].equals(o)){
+                    Object[] arr = new Object[size-i-1];
+                    for (int j = 0; j < size-i-1 ; j++) {
+                        arr[j] = list[i+j+1];
+                    }
+                    for (int j = 0; j < size-i-1; j++) {
+                        list[i+j] = arr[j];
+                    }
+                    list[size-1] = null;
+                    size--;
+                    return true;
+                }
+            }
         }
         return false;
     }
+    @Override
+    public Iterator<T> iterator() {
+        //Iterator newArray = new Iterable<>();
+        return null;
+    }
+
+
+
+
+
 
     @Override
     public boolean containsAll(Collection<?> c) {
@@ -80,12 +175,12 @@ public class MyList<T> implements List<Object> {
     }
 
     @Override
-    public boolean addAll(Collection<?> c) {
+    public boolean addAll(Collection<? extends T> c) {
         return false;
     }
 
     @Override
-    public boolean addAll(int index, Collection<?> c) {
+    public boolean addAll(int index, Collection<? extends T> c) {
         return false;
     }
 
@@ -104,56 +199,31 @@ public class MyList<T> implements List<Object> {
 
     }
 
-    @Override
-    public T get(int index) {
-        T t = null;
-        if (index >= 0 & index < list.length) {
-            for (Item<T> item : list) {
-                if (item.getCurrent() == index) {
-                    t = item.getItem();
-                }
-            }
-        }
-        return t;
-    }
+
+
+
+
+
 
     @Override
-    public Object set(int index, Object element) {
+    public T remove(int index) {
+        return null;
+    }
+
+
+
+    @Override
+    public ListIterator<T> listIterator() {
         return null;
     }
 
     @Override
-    public void add(int index, Object element) {
-
-    }
-
-    @Override
-    public Object remove(int index) {
+    public ListIterator<T> listIterator(int index) {
         return null;
     }
 
     @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public ListIterator<Object> listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator<Object> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<Object> subList(int fromIndex, int toIndex) {
+    public List<T> subList(int fromIndex, int toIndex) {
         return null;
     }
 
